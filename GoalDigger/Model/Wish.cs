02cf690c6671e31fe3f8466908a95d3c654b4d42
gwake1 <gwake1@gmail.com>
2015-02-14 13:37:15 -0600

    GoalDigger.Model && Test Helper

---
+++ GoalDigger/Model/Wish.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoalDigger.Model
+{
+    public class Wish
+    {
+        public Wish(string WishName, string WishDate, int Price)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
