4be7d232283356708ad8f1d10f733cf6d77c2d1d
gwake1 <gwake1@gmail.com>
2015-02-14 12:08:17 -0600

    Initial Zero State Tests

---
+++ TestGoalDigger/ZeroStateTest.cs
@@ -0,0 +1,53 @@
+﻿﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestStack.White.UIItems.WindowItems;
+using TestStack.White;
+using System.IO;
+using TestStack.White.Factory;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.ListBoxItems;
+using TestStack.White.UIItems.Finders;
+
+namespace TestGoalDigger
+{
+    [TestClass]
+    public class ZeroStateTest
+    {
+
+        private static TestContext test_context;
+        private static Window window;
+        private static Application application;
+
+        [ClassInitialize]
+        public static void Setup(TestContext _context)
+        {
+            test_context = _context;
+            var applicationDir = _context.DeploymentDirectory;
+            var applicationPath = Path.Combine(applicationDir, "..\\..\\..\\GoalDigger\\bin\\Debug\\GoalDigger");
+            application = Application.Launch(applicationPath);
+            window = application.GetWindow("MainWindow", InitializeOption.NoCache);
+        }
+
+        [TestMethod]
+        public void TestZeroStateAddWishButton()
+        {
+            Button button = window.Get<Button>("AddWish");
+            Assert.AreEqual("Add to Wish List", button.Text);
+        }
+
+        [TestMethod]
+        public void TestZeroStateDefaultWishList()
+        {
+            ListBox countdowns = window.Get<ListBox>("WishList");
+            Assert.AreEqual(1, countdowns.Items.Count);
+            Assert.AreEqual("SurfBoard", countdowns.Items[0].Text);
+        }
+
+        [ClassCleanup]
+        public static void CleanThisUp()
+        {
+            window.Close();
+            application.Close();
+        }
+    }
+}
